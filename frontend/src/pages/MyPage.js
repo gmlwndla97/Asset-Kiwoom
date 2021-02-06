17b545412b25f9ff65d24c956f0c58f8bad49b6c
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2021-02-06 21:45:00 +0900

    edit MyPage UI

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import css from 'styled-components';
+import styled from 'styled-components';
 
 function MyPage() {
-  const SelectionBar = css.div`
-  width: 500px;
+  const SelectionBar=css.div`
   border-radius: 5px;
-  height: 100px;
+  float:left;
+  padding-top:20px;
   padding-left:20px;
-  float: left;
+  `
+  const ButtonLI = styled.button`
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  height: 2.5rem;
+  font-size: 1rem;
+  background: #A9D0F5;
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
   `
 
   const Box = css.div`
-  width: 500px;
+  width: 1000px;
   margin-top:20px;
   padding-left:20px;
   height: 500px;
+  position: relative;
+  align:center;
   float: left;
   `
 
@@ -28,8 +50,7 @@ function MyPage() {
   `
 
 
-
-  const History=css.div`
+ const History=css.div`
   background-color: #F0F0F0;
   padding: 1rem;
   border-radius: 5px;
@@ -42,7 +63,6 @@ function MyPage() {
 
   const UL = css.ul`
   list-style-type: none;
-  background-color: #F0F0F0;
   padding: 1rem;
   border-radius: 5px;
   `
@@ -58,14 +78,26 @@ function MyPage() {
   `
 
   return (
-    <div>
+    <div style={{width: '75vw', float: 'left', 'border-spacing': '20px'}}>
       <SelectionBar>
-         <UL>
-            <LI> <a href="#">선택1 </a></LI>
-            <LI>  <a href="#">선택2 </a></LI>
-            <LI> <a href="#"> 선택3 </a></LI>
-          </UL>
+        <ButtonLI><input type="radio" value="notSigned" id="notSigned" name="selection"/> 
+        <label for="notSigned"> 미체결 </label></ButtonLI>
+
+        <ButtonLI><input type="radio" value="balance" id="balance" name="selection" />
+        <label for="balance">잔고</label> </ButtonLI>
+
+        <ButtonLI><input type="radio" value="deposit" name="selection"id="selection" />
+        <label for="selection">예수금</label> </ButtonLI>
+
+        <ButtonLI><input type="radio" value="todayTrading" id="todayTrading" name="selection" /> 
+        <label for="todayTrading">당일매매</label></ButtonLI>
+        
+        <ButtonLI><input type="radio" value="confirm" id="confirm" name="selection" />
+        <label for="confirm">체결확인</label> </ButtonLI>
       </SelectionBar>
+       
+        
+      
       <Box>
         <Filter>
           필터
