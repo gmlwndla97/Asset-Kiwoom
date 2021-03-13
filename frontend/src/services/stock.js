221342340061b08d9e970db4195b922afb5e7c3f
dks8064 <dks8064@naver.com>
2021-03-13 15:08:33 +0900

    add stockChart

--- frontend/src/services/stock.js
+++ frontend/src/services/stock.js
@@ -1,4 +1,5 @@
 import Axios from '../core/Axios';
+import Socket from '../core/Socket';
 
 function getStock(value, callback) {
     Axios('stock/current', value, callback);
@@ -8,8 +9,8 @@ function getFavoriteStock(value, callback) {
     Axios('stock/favorite', value, callback);
 }
 
-function getRealStock(value, callback) {
-    Axios('stock/real', value, callback);
+function getRealStock(callback) {
+    return Socket('stock/real', callback);
 }
 
 export {
