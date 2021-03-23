8096741960c124e508bab9ec4ef5e732cf0d6a1f
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2021-03-24 00:17:25 +0900

    fix axios port number

--- frontend/src/core/Axios.js
+++ frontend/src/core/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function Axios(url, param, callback) {
     axios({
         method: 'post',
-        url: 'http://127.0.0.1:8080/' + url + '/',
+        url: 'http://127.0.0.1:8000/' + url + '/',
         params: param,
         withCredentials: true
     }).then(response => callback(response.data))
